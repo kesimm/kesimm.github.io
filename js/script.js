87a05540dc2f1055085e25e918868787640beac8
kz1127 <kenzie.simmons27@gmail.com>
2018-06-19 15:30:08 -0600

    adding mailto

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@
     }); // end of document ready
   })(jQuery); // end of jQuery name space
   
+
+  ('#send').click(function() {
+    $('#form').attr('action',
+                   'mailto:kenzie.simmons27@gmail.com?subject=' +
+                   $('#name').val() + $('#email').val() + '&body=' + $('#tb2').val());
+    $('#form').submit();
+});
